14fba8bff572fc0c0f5fcca4c42ec9e780f5cde6
Halimo9812 <Halimoaali98@gmail.com>
2024-08-22 15:46:16 -0400

    first commit

---
+++ Adventure.js
@@ -0,0 +1,29 @@
+// Part 1: Humble Beginnings
+// Let’s model a simple adventurer with basic properties such as health and an inventory. 
+// We will call the adventurer “Robin.”
+
+const adventurer = {
+name: "Robin",
+health: 10,
+inventory: ["sword", "potion", "artifact"]
+
+//Nested arrays are useful, but so are nested objects. Let’s give Robin a companion to travel with – a furry friend they call “Leo.”
+companion:{
+    name: "Leo",
+    type: "Cat"
+
+    companion.subSubject: 
+}
+}
+
+
+// Next, give Robin’s feline friend a friend of his own:
+// Add a “companion” sub-object to “Leo” with the following properties:
+// The companion’s name is “Frank.”
+// The companion’s type is “Flea.”
+// The companion has its own belongings, which includes a small hat and sunglasses.
+
+
+
+
+
